ad174f407a4ff8bfaff954a2bfc25cd870188b95
Mikita Sasim <mikita.sasim@student.ehu.lt>
2025-04-06 22:43:53 +0200

    done

---
+++ SingletonExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using SingletonExample;
+
+var a = TimeStampSingleton.GetInstance();
+Thread.Sleep(1000);
+
+var b = TimeStampSingleton.GetInstance();
+Thread.Sleep(1000);
+
+var c = TimeStampSingleton.GetInstance();
+Thread.Sleep(1000);
+
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
---
+++ SingletonExample/TimeStampSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace SingletonExample
+{
+    public sealed class TimeStampSingleton
+    {
+        private static readonly TimeStampSingleton instance = new TimeStampSingleton();
+
+        public static TimeStampSingleton GetInstance() => instance;
+
+        private readonly DateTime creationTime;
+
+        private TimeStampSingleton()
+        {
+            creationTime = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return creationTime.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
+
+
